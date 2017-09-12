7cf9e46b1e7dc3b2d7bef658ee654ce986b663f9
RiEn <15ts0044@esp-g.com>
2017-09-12 17:32:33 +0900

    Ptra03_01

--- Practice/src/practice03/PTra03_01.java
+++ Practice/src/practice03/PTra03_01.java
@@ -5,14 +5,13 @@ package practice03;
  *------------------------------------------------------------
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
-
 public class PTra03_01 {
 	public static void main(String[] args) {
 		// ★ int型の変数numを宣言し、70で初期化してください
-
+		int num = 70;
 
 		// ★ 「変数numが70である」という条件式を追加してください
-		if() {
+		if(num == 70) {
 			System.out.println("条件に合致しました");
 		}
 	}
